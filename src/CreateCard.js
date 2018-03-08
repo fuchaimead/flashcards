6b130ee8b247e6032a8c57b7f78a3d1d60f489b2
FuChai Mead <mead.fuchai@gmail.com>
2018-03-07 23:21:44 -0700

    attempting edit

--- src/CreateCard.js
+++ src/CreateCard.js
@@ -1,22 +1,22 @@
 import React from 'react';
 
 class CreateCard extends React.Component {
-state = {question: '', answer: '', id: 4}
+state = {question: '', answer: '', id: 0}
 
 createCard = (e) => {
   e.preventDefault()
   const {question, answer, id} = this.state;
   this.setState({ id: id + 1})
   const card = {question, answer, id}
-  if (card !== null) {
   this.props.createCard(card);  
   this.setState({ question: '', answer: ''});
-  }
 }
 
 handleChange = (e) => {
   let {name, value } = e.target;
   this.setState({ [name]: value })
+  // this.state['front']
+  // this.state.front are equivalent 
  
 }
 
@@ -26,8 +26,20 @@ render(){
     <div> 
       <p> Add your own question </p>
     <form onSubmit={this.createCard}>
-      <input name="question" value={question} placeholder="write a question" onChange={this.handleChange} />
-      <input name="answer" value={answer} placeholder="write an answer" onChange={this.handleChange} />
+      <input 
+        name="question" 
+        value={question} 
+        placeholder="write a question"
+        required={true} 
+        onChange={this.handleChange} 
+        />
+      <input 
+        name="answer" 
+        value={answer} 
+        placeholder="write an answer" 
+        required={true} 
+        onChange={this.handleChange} 
+        />
       <button type="submit"> Create Card </button>
     </form>
     </div>
--- src/FlashCard.js
+++ src/FlashCard.js
@@ -4,13 +4,15 @@ import CreateCard from './CreateCard';
 class FlashCard extends React.Component {
   state = {
     cards: [
-      {question: 'What is React?', answer:'React is a library that makes it easy to design a UI that is data driven.' },
+      {question: 'What is React?', answer:'React is a library that makes it easy to design a UI that is data driven.'},
       {question:'What is state?', answer: 'State is reserved for data that can be changed over time.'}, 
       {question: 'What are props?', answer: 'Props are properties that are used to pass data from parent to children.'},
       {question:'Who uses React?', answer:'Facebook and many other websites. React was invented by a developer who worked at Facebook to render data as HTML elements.'}
     ],
     showing: [],
     isShowing: false,
+    editing: [],
+    isEditing: false,
   }
 
 addCard = (card) => {
@@ -22,8 +24,8 @@ showAnswer(i) {
   this.setState({ isShowing: !isShowing, showing: [i]})
 }
 
-deleteCard(i) {
-  alert('Are you sure you want to delete?')
+deleteCard = (i) => {
+  window.confirm('Are you sure you want to delete?')
   const {cards} = this.state
   cards.splice(i, 1)
   this.setState({card: cards}) 
@@ -31,16 +33,30 @@ deleteCard(i) {
 }
 
 editCard(i){
-  
+  const { isEditing, editing } = this.state
+  this.setState({ isEditing: !isEditing, editing: [i] }) 
 }
 
-createCard() {
- console.log("hello")
+updateCard = (e) => {
+  e.preventDefault()
+  let cards = this.state.cards.map( c => {
+    if (this.state.editing === this.state.card)
+      return this.state.card;
+    return c;
+  })
+  this.setState({ cards });
 }
 
+
+handleChange = (e) => {
+  let {name, value } = e.target;
+  this.setState({ [name]: value })
+  // this.state['front']
+  // this.state.front are equivalent 
+}
 
 render(){
-  const {isShowing, showing} = this.state
+  const {isShowing, showing, isEditing, editing} = this.state
   return ( 
     <div>
       <CreateCard createCard={this.addCard} />
@@ -51,21 +67,38 @@ render(){
             <h4><strong>{card.question}</strong></h4>
             <p>{card.answer}</p>
             <button onClick={() => this.showAnswer(i)}>Close Answer</button>
-            <button onClick={() => this.deleteCard(i)}> Delete </button>
-             <button onClick={() => this.editCard(i)}> Edit </button>
+            <button onClick={() => this.deleteCard(i)}>Delete</button>
+            <button onClick={() => this.editCard(i)}>Edit</button>
             </div>
            )
-         } else {
+         } else if (isEditing && editing.includes(i)) {
             return(
+            <div key={i}>
+             <form onSubmit={this.updateCard}>
+              <input 
+                name="question" 
+                value={this.state.question}
+                onChange={this.handleChange} 
+                />
+              <input 
+                name="answer" 
+                value={this.state.answer} 
+                onChange={this.handleChange} 
+                />
+              <button type="submit"> Update Card </button>
+            </form>
+            </div>
+          ) 
+         } else {
+          return(
             <div key={i}>
             <h4><strong>{card.question}</strong></h4>
             <button onClick={() => this.showAnswer(i)}>Show Answer</button>
-            <button onClick={() => this.deleteCard(i)} > Delete </button>
-             <button onClick={() => this.editCard(i)}> Edit </button>
+            <button onClick={() => this.deleteCard(i)}>Delete</button>
+             <button onClick={() => this.editCard(i)}>Edit</button>
             </div>
           ) 
-         }  
-        
+         }      
       })}
     </div>
    )
